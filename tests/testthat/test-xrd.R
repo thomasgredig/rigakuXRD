29ee78e90feb4e428785b9ba2040d625ad0b3682
Thomas Gredig <thomas.gredig@csulb.edu>
2019-05-26 18:29:51 -0700

    add lines for better debugging

--- tests/testthat/test-xrd.R
+++ tests/testthat/test-xrd.R
@@ -1,5 +1,6 @@
 test_that("XRD reader", {
   filename = system.file("extdata", "MnPcTheta.txt", package='rigakuXRD')
+  # filename=file.path(getwd(),'inst/extdata','MnPcTheta.txt')
   d = xrd.read.TXTnoheader(filename)
   d = d[[1]]
 

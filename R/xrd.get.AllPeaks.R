6144e80607ea5ac4f8470c82f8b3674c70742217
Thomas Gredig <thomas.gredig@csulb.edu>
2019-05-28 21:58:33 -0700

    decrease step size and make prominence variable

--- R/xrd.get.AllPeaks.R
+++ R/xrd.get.AllPeaks.R
@@ -3,6 +3,7 @@ empty.list = c()
 #'
 #' @param TwoTheta angle
 #' @param Intensity intensity signal
+#' @param min.Prominence minimum prominence in percent (optional)
 #' @param Try.Sigma vector with peak widths used to start fitting (optional)
 #' @param Range range to search for peaks (optional)
 #' @return vector with peak positions
@@ -14,9 +15,10 @@ empty.list = c()
 #'
 #' @export
 xrd.get.AllPeaks <- function(TwoTheta, Intensity,
+                             min.Prominence = 5,
                              Try.Sigma = c(0.1,0.2,0.15),
                              Range = c(0,90)) {
-  step.size = 0.7 # search peaks with this step size
+  step.size = 0.4 # search peaks with this step size
   if (Range[1]==0) { theta.min = min(TwoTheta) + step.size/2 }
   if (Range[2]==90) { theta.max = max(TwoTheta) - step.size/2 }
   if ((theta.max-theta.min)<1) { return(empty.list) }
@@ -24,10 +26,9 @@ xrd.get.AllPeaks <- function(TwoTheta, Intensity,
 
   p = c()
   for(ang in peakPos.list) {
-    #p1 = xrd.find.Peak(TwoTheta, Intensity, ang)
     p1 = xrd.get.PeakStats(TwoTheta, Intensity, ang)
-    if(!is.na(p1)) {
-      if (xrd.get.PeakProminence(p1)>10) {
+    if(length(p1)==8) {
+      if (xrd.get.PeakProminence(p1) > min.Prominence) {
         p = c(p, p1[3])
       }
     }

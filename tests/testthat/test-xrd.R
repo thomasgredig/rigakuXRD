1cf95a14f441f09d2eadb830c1ae9895af8ab1a8
Thomas Gredig <thomas.gredig@csulb.edu>
2020-02-29 10:39:18 -0800

    update testing

--- tests/testthat/test-xrd.R
+++ tests/testthat/test-xrd.R
@@ -1,7 +1,6 @@
 test_that("XRD reader", {
   filename = system.file("extdata", "MnPcTheta.txt", package='rigakuXRD')
-  # filename=file.path(getwd(),'inst/extdata','MnPcTheta.txt')
-  d = xrd.read.TXTnoheader(filename)
+  d = xrd.read.TXT(filename)
   d = d[[1]]
 
   expect_equal(nrow(d), 2201)
